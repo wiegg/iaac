987e9b404b24b7fda0fcd8b4441cf276659d9518
Felix Wieland <mail@felixwie.com>
2021-10-31 00:19:55 +0200

    added redis

--- inventory.js
+++ inventory.js
@@ -2,6 +2,9 @@ const fs = require("fs")
 const yaml = require("yaml")
 
 const raw = fs.readFileSync("state.json")
+
+console.log(raw.toString())
+
 const data = JSON.parse(raw.toString())
 ip_list = {}
 
