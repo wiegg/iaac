bce1da15846bdf1bd129b66cabb100ba4b78d8d0
Felix Wieland <mail@felixwie.com>
2021-10-31 00:15:22 +0200

    switched to nodejs

---
+++ inventory.js
@@ -0,0 +1,22 @@
+const fs = require("fs")
+const yaml = require("yaml")
+
+const raw = fs.readFileSync("state.json")
+const data = JSON.parse(raw.toString())
+ip_list = {}
+
+data["resources"].forEach(i => {
+  if (i["type"] == "hcloud_server" && i["mode"] == "managed") {
+    i["instances"].forEach(s => {
+      ip_list[s["attributes"]["ipv4_address"]] = null
+    })
+  }
+});
+
+const inventory = {
+  nodes: {
+    hosts: ip_list
+  }
+}
+
+console.log(yaml.stringify(inventory))
