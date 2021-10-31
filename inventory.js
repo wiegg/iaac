5c0553692af2ce12a3c5c22d844029562ad3fef6
Felix Wieland <mail@felixwie.com>
2021-10-31 10:30:55 +0100

    changed ssh key

--- inventory.js
+++ inventory.js
@@ -3,19 +3,27 @@ const yaml = require("yaml")
 
 const data = JSON.parse(process.env.STATE.toString())
 
-ip_list = {}
+ip_list_nodes = {}
+ip_list_redis = {}
 
 data["resources"].forEach(i => {
   if (i["type"] == "hcloud_server" && i["mode"] == "managed") {
     i["instances"].forEach(s => {
-      ip_list[s["attributes"]["ipv4_address"]] = null
+      ip_list_nodes[s["attributes"]["ipv4_address"]] = null
+
+      if(s["attributes"]["labels"]["redis"]) {
+        ip_list_redis[s["attributes"]["ipv4_address"]] = null
+      }
     })
   }
 });
 
 const inventory = {
   nodes: {
-    hosts: ip_list
+    hosts: ip_list_nodes
+  },
+  redis: {
+    hosts: ip_list_redis
   }
 }
 
